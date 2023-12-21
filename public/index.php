1ce396172e3a6939c1b4f64d93c2e62d6314ed14
SultanAfrizal <Sltnafrizal@gmail.com>
2023-12-21 17:12:41 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php require "layout/head.php";?>
+
+    <body>
+        <div id="app">
+            <?php require "layout/sidebar.php";?>
+            <div id="main">
+                <header class="mb-3">
+                    <a href="#" class="burger-btn d-block d-xl-none">
+                        <i class="bi bi-justify fs-3"></i>
+                    </a>
+                </header>
+                <div class="page-heading">
+                    <h3>Dashboard</h3>
+                </div>
+                <div class="page-content">
+                    <section class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>Sistem Pendukung Keputusan Pegawai Terbaik</h4>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <p class="card-text">
+                                            kelopok 6
+                                            <li>1955201111 AHMAD SOULTAN AFRIZAL</i>
+                                            <li>1855201402 Muhammad Rizqi FernandaL</i>
+                                            <li>2055201261 FEGI FERINSA SYAFAAT</i>
+                                        </p>
+                                        <hr>
+                                        <p class="card-text">
+                                            Langkah Penyelesaian Simple Additive Weighting (SAW) adalah sebagai berikut
+                                            :
+                                        </p>
+                                        <ol type="1">
+                                            <li>Menentukan kriteria-kriteria yang akan dijadikan acuan dalam pengambilan
+                                                keputusan, yaitu Ci</i>
+                                            <li>Menentukan rating kecocokan setiap alternatif pada setiap kriteria (X).
+                                            </li>
+                                            <li>Membuat matriks keputusan berdasarkan kriteria(Ci), kemudian melakukan
+                                                normalisasi matriks berdasarkan persamaan yang disesuaikan dengan jenis
+                                                atribut (atribut keuntungan ataupun atribut biaya) sehingga diperoleh
+                                                matriks ternormalisasi R.</li>
+                                            <li>Hasil akhir diperoleh dari proses perankingan yaitu penjumlahan dari
+                                                perkalian matriks ternormalisasi R dengan vektor bobot sehingga
+                                                diperoleh nilai terbesar yang dipilih sebagai alternatif terbaik
+                                                (Ai)sebagai solusi</li>
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <?php require "layout/footer.php";?>
+            </div>
+        </div>
+        <?php require "layout/js.php";?>
+    </body>
+
+</html>
---
+++ public/layout/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2023 &copy; SPK - SAW Kelompok 6</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                    href="https://www.instagram.com/sultan_afrizal_/">@sultan_afrizal</a></p>
+        </div>
+    </div>
+</footer>
